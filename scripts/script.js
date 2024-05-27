fd4d79cd74a42b68d2ccb163c8cd397223f265f5
marclod <marcleidsoncontato@gmail.com>
2024-05-27 17:15:49 -0300

    added: navbar and create home.html

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,20 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("back").classList.add("disable-mouse-events")
     });     
 });
+
+let boxBuscar = document.querySelector('.buscar-box');
+let lupa = document.querySelector('.lupa-buscar')
+let btnFechar = document.querySelector('.btn-fechar')
+
+lupa.addEventListener('click', () => {
+    boxBuscar.classList.add('ativar');
+    btnFechar.classList.remove('hidden');
+});
+
+btnFechar.addEventListener('click', () => {
+    boxBuscar.classList.remove('ativar');
+    btnFechar.classList.add('hidden');
+    setTimeout(() => {
+        boxBuscar.classList.remove('ativar');
+    }, 300);
+});
