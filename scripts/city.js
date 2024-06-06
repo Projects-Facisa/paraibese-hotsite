fc0eec7c684db5cd560911a07e968ec3a97f5148
medeiroscaio <caiovictor.medeiros51@gmail.com>
2024-06-05 23:59:38 -0300

    added: hover on icons, javascript to show and hide sections.

---
+++ scripts/city.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const iconElements = document.getElementsByClassName("view-icon");
+
+  Array.from(iconElements).forEach((iconElement) => {
+    iconElement.addEventListener("mouseover", () => {
+      iconElement.classList.remove("fa-regular");
+      iconElement.classList.add("fa-solid");
+    });
+
+    iconElement.addEventListener("mouseout", () => {
+      iconElement.classList.remove("fa-solid");
+      iconElement.classList.add("fa-regular");
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.showSection = function (sectionId) {
+    var sections = document.getElementsByClassName("section");
+    for (var i = 0; i < sections.length; i++) {
+      sections[i].style.display = "none";
+    }
+    var sectionToShow = document.getElementById(sectionId);
+    sectionToShow.style.display = "grid";
+
+    var carousels = sectionToShow.getElementsByClassName("carousel");
+    for (var j = 0; j < carousels.length; j++) {
+      var flkty = Flickity.data(carousels[j]);
+      if (flkty) {
+        flkty.resize();
+      }
+    }
+  };
+
+  var carousels = document.querySelectorAll(".carousel");
+  carousels.forEach(function (carousel) {
+    new Flickity(carousel, {
+      autoPlay: true,
+      wrapAround: true,
+    });
+  });
+
+  window.showHide = function () {
+    alert("Esta funcionalidade ainda está em desenvolvimento.");
+  };
+});
