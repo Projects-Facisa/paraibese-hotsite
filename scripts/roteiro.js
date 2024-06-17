600f4958ae2e271f63e62b7defd7a970f6bdced1
medeiroscaio <caiovictor.medeiros51@gmail.com>
2024-06-17 01:05:23 -0300

    added: eventos section.

--- scripts/roteiro.js
+++ scripts/roteiro.js
@@ -1,30 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('.timeline-details a');
-  
-    links.forEach(link => {
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-        
-        const targetId = this.getAttribute('href').substring(1);
-        const targetSpan = document.getElementById(targetId);
-  
-        if (targetSpan && targetSpan.classList.contains('roteiro-place')) {
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".timeline-details a");
 
-          targetSpan.classList.remove('brilhar');
+  links.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
 
-          void targetSpan.offsetWidth;
-  
-          targetSpan.classList.add('brilhar');
+      const targetId = this.getAttribute("href").substring(1);
+      const targetSpan = document.getElementById(targetId);
 
-          const targetPosition = targetSpan.getBoundingClientRect().top + window.pageYOffset;
-          const offsetPosition = targetPosition - (window.innerHeight / 2) + (targetSpan.offsetHeight / 2);
-  
-          window.scrollTo({
-            top: offsetPosition,
-            behavior: 'smooth'
-          });
-        }
-      });
+      if (targetSpan && targetSpan.classList.contains("roteiro-place")) {
+        targetSpan.classList.remove("brilhar");
+
+        void targetSpan.offsetWidth;
+
+        targetSpan.classList.add("brilhar");
+
+        const targetPosition =
+          targetSpan.getBoundingClientRect().top + window.pageYOffset;
+        const offsetPosition =
+          targetPosition - window.innerHeight / 2 + targetSpan.offsetHeight / 2;
+
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: "smooth",
+        });
+      }
     });
   });
-  
+});
